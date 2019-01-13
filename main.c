a7c4792373f7e5a812844b1836922fe826f8f94f
maksged <maksged@gmail.com>
2019-01-13 22:10:03 +0200

    menu in alpha

--- main.c
+++ main.c
@@ -38,25 +38,30 @@ void		intro_anim(t_wolf *wolf, t_texture *intro)
 	int	x;
 	int	y;
 	int	count;
+	SDL_Event e;
 
 	count = 256;
-	while (count > 0)
+	while (--count > 0)
 	{
-		count -= 1;
-		y = 199;
+		y = -1;
 		while (++y < SCREEN_HEIGHT)
 		{
-			x = 199;
-			while (++x < 900)
+			x = -1;
+			while (++x < SCREEN_WIDTH)
 			{
-				get_color(intro, &wolf->color, x, y);
-				wolf->color.r = wolf->color.r > 0 ? (Uint8)(wolf->color.r * (count / 255.0)) : 0;
-				wolf->color.g = wolf->color.g > 0 ? (Uint8)(wolf->color.g * (count / 255.0)) : 0;
-				wolf->color.b = wolf->color.b > 0 ? (Uint8)(wolf->color.b * (count / 255.0)) : 0;
+				wolf->color.r = (wolf->buff[y * SCREEN_WIDTH + x] >> 16 & 0xFF) > 0 ?
+						(wolf->buff[y * SCREEN_WIDTH + x] >> 16 & 0xFF) - 1 : 0;
+				wolf->color.g = (wolf->buff[y * SCREEN_WIDTH + x] >> 8 & 0xFF) > 0 ?
+						(wolf->buff[y * SCREEN_WIDTH + x] >> 8 & 0xFF) - 1 : 0;
+				wolf->color.b = (wolf->buff[y * SCREEN_WIDTH + x] & 0xFF) > 0 ?
+						(wolf->buff[y * SCREEN_WIDTH + x] & 0xFF) - 1 : 0;
 				set_pixel_s(wolf, &wolf->color, x, y);
 			}
 		}
 		screen_upd(wolf);
+		if (SDL_PollEvent(&e))
+			if (e.type == SDL_KEYDOWN && e.key.keysym.scancode == SDL_SCANCODE_SPACE)
+				break;
 	}
 }
 
@@ -74,6 +79,7 @@ void		intro(t_wolf *wolf)
 	intro_anim(wolf, &intro);
 	destroy_texture(&intro);
 	
+	/*printf("done\n");
 	SDL_Event event;
 	while (1)
 	{
@@ -82,6 +88,41 @@ void		intro(t_wolf *wolf)
 			if (event.type == SDL_QUIT)
 				return ;
 		}
+	}*/
+}
+
+int pause_with_break(Uint32 pause)
+{
+	static SDL_Event	event;
+
+	while (pause-- > 0)
+	{
+		if (SDL_PollEvent(&event))
+			if (event.type == SDL_QUIT)
+				return (1);
+		SDL_Delay(1);
+	}
+	return (0);
+}
+
+void menu(t_wolf *wolf)
+{
+	t_texture menu[2];
+
+	load_texture(wolf, &menu[0], "resource/img/menu/bg-1.jpg");
+	printf("1\n");
+	load_texture(wolf, &menu[1], "resource/img/menu/bg-2.jpg");
+	printf("2\n");
+	while (1)
+	{
+		cp_tex_to_buff(wolf, &menu[0]);
+		screen_upd(wolf);
+		if (pause_with_break(500))
+			break ;
+		cp_tex_to_buff(wolf, &menu[1]);
+		screen_upd(wolf);
+		if (pause_with_break(500))
+			break;
 	}
 }
 
@@ -93,6 +134,7 @@ int			main(int ac, char **av)
 	//SDL_ShowCursor(SDL_DISABLE);
 	//SDL_WarpMouseInWindow(wolf->win, SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2);
 intro(wolf);
+menu(wolf);
 	// raycast(wolf);
 
 	//close_win(wolf);
